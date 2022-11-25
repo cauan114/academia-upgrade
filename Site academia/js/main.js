41f9e2cae6c427e4d3325ac9afa07f5115bafef3
Agnasha Ellis <112880845+Agnasha@users.noreply.github.com>
2022-11-24 23:13:52 -0300

    Adicionada pagina exercicios
    Exewrcicios de layout ja pronto...
    
    Falta adicionar os videos de treinamento
    
    Muitas bibliotecas  foram acrescentadas

--- Site academia/js/main.js
+++ Site academia/js/main.js
@@ -1,29 +1,174 @@
-//Calculadora//
+/*  ---------------------------------------------------
+  Template Name: Gym
+  Description:  Gym Fitness HTML Template
+  Author: Colorlib
+  Author URI: https://colorlib.com
+  Version: 1.0
+  Created: Colorlib
+---------------------------------------------------------  */
 
-let botaoCalcular = document.getElementById(btn_calcular);
-function calculandoIMC(){
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value/100;
-    let resultado = document.getElementById("resultado")
-    if(altura !== "" && peso !==""){
-        let imc = (peso / (altura*altura)).toFixed(2);
-        let mensagem = "";
-        if(imc < 18.5){
-            mensagem = "Abaixo do peso!"
-        }else if(imc < 25){
-            mensagem = "Você está com o peso ideal!"
-        }else if(imc < 30){
-            mensagem = "Você está levemente acima do peso!"
-        }else if(imc < 35){
-            mensagem = "Cuidado! Obesidade grau I"
-        }else if(imc < 40){
-            mensagem = "Cuidado! Obesidade grau II"
-        }else {
-            mensagem = "Cuidado! Obesidade grau III"
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    //Masonary
+    $('.gallery').masonry({
+        itemSelector: '.gs-item',
+        columnWidth: '.grid-sizer',
+        gutter: 10
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Carousel Slider
+    --------------------*/
+    var hero_s = $(".hs-slider");
+    hero_s.owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false
+    });
+
+    /*------------------
+        Team Slider
+    --------------------*/
+    $(".ts-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        dotsEach: 2,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 3,
+            }
         }
-        resultado.textContent = `Seu IMC é ${imc}! ${mensagem}`;      
-    }else{
-        resultado.textContent = "Preencha todos os campos!!!"
-    }
-}
-btn_calcular.addEventListener('click', calculandoIMC);
+    });
+
+    /*------------------
+        Testimonial Slider
+    --------------------*/
+    $(".ts_slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*------------------
+        Image Popup
+    --------------------*/
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+    /*------------------
+        Video Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+        Barfiller
+    --------------------*/
+    $('#bar1').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar2').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+    $('#bar3').barfiller({
+        barColor: '#ffffff',
+        duration: 2000
+    });
+
+    $('.table-controls ul li').on('click', function () {
+        var tsfilter = $(this).data('tsfilter');
+        $('.table-controls ul li').removeClass('active');
+        $(this).addClass('active');
+
+        if (tsfilter == 'all') {
+            $('.class-timetable').removeClass('filtering');
+            $('.ts-meta').removeClass('show');
+        } else {
+            $('.class-timetable').addClass('filtering');
+        }
+        $('.ts-meta').each(function () {
+            $(this).removeClass('show');
+            if ($(this).data('tsmeta') == tsfilter) {
+                $(this).addClass('show');
+            }
+        });
+    });
+
+})(jQuery);
